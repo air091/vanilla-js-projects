07a048a4b5a90969847939f5d329664885369942
Air <Acesparkle243@gmail.com>
2025-04-01 16:47:13 +0800

    update title and Responsive Sidebar

---
+++ 5. Responsive Sidebar/script.js
@@ -0,0 +1,7 @@
+let sidebar = document.getElementById("container");
+let menu = document.querySelector("#out .bx-menu");
+
+menu.addEventListener("click", function () {
+    sidebar.classList.toggle("active");
+
+});
